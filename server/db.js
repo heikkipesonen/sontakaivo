eff26a1735cbde6e9980aac489f48be9b2791895
Heikki Pesonen <pesonen.heikki@gmail.com>
2017-03-17 14:04:13 +0200

    autocommit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('shitwell', 'root', 'kissakala', {
+const db = new Sequelize('shitwell', 'shitwell', 'kaivo', {
   dialect: 'mysql',
   host: 'localhost',
   port: 3306
