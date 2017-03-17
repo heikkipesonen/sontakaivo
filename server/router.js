bd6fbd568ef9bd24a615ba4ede44270c3cf5e0c7
Heikki Pesonen <pesonen.heikki@gmail.com>
2017-03-17 15:05:23 +0200

    autocommit

--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/measured', (req, res) => {
-    const start = req.param('start') || Date.now() - 24 * 60 * 60 * 1000;
+    const start = req.param('start') || (Date.now() - 24 * 60 * 60 * 1000);
     const end = req.param('end') || Date.now();
     const offset = parseInt(req.param('offset')) || 0;
     const limit = parseInt(req.param('limit')) || 100;
