fe351985f142b82682d8cf768ea96c7815f1d0de
Heikki Pesonen <pesonen.heikki@gmail.com>
2017-03-17 21:01:39 +0200

    autocommit

--- server/server.js
+++ server/server.js
@@ -18,12 +18,7 @@ server.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+  next();
 });
 
 server.options("/*", function(req, res, next){
